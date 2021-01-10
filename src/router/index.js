98ea40a9f6cb89e3cbbc86bd951dc849107c4f47
willfpinheiro <42159903+willfpinheiro@users.noreply.github.com>
2021-01-10 11:26:50 -0300

    creat all

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,51 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
+import Brazil from '../views/Brazil.vue';
+import Hawaii from '../views/Hawaii.vue';
+import Jamaica from '../views/Jamaica.vue';
+import Panama from '../views/Panama.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+	{
+		path: '/',
+		name: 'Home',
+		component: Home,
+	},
+	{
+		path: '/about',
+		name: 'About',
+		// route level code-splitting
+		// this generates a separate chunk (about.[hash].js) for this route
+		// which is lazy-loaded when the route is visited.
+		component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+	},
+	{
+		path: '/brazil',
+		name: 'brazil',
+		component: () => import('../views/Brazil.vue'),
+	},
+	{
+		path: '/jamaica',
+		name: 'jamaica',
+		component: () => import('../views/Jamaica.vue'),
+	},
+	{
+		path: '/panama',
+		name: 'panama',
+		component: () => import('../views/Panama.vue'),
+	},
+	{
+		path: '/hawaii',
+		name: 'hawaii',
+		component: () => import('../views/Hawaii.vue'),
+	},
 ];
 
 const router = new VueRouter({
-  routes
+	routes,
 });
 
 export default router;
